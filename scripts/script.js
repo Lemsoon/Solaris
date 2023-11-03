1111849a2f8f6aec7e2d82c4fcf9d898c15d2314
Sebastian <selfishworm117@gmail.com>
2023-11-03 17:13:42 +0100

    dasbj

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/*temp until api works
+egenskap	    datatyp	  enhet
+id	          number	  -
+type	        string	  star & planet
+name	        string	  namnet på himlakroppen
+latinName	    string	  Latinska namnet på himlakroppen
+rotation	    number	  Längd på dygn i antal jorddygn runt sin egen axel
+circumference	number	  Omkrets i km
+temp	        Object	  Temperatur day och night i celcius.
+distance	    number	  km från solen
+orbitalPeriod	Number	  Antal jorddygn runt solen
+desc	        string	  Beskrivning av himlakroppen
+moons	        Array	    Lista med månarnas namn
+*/
